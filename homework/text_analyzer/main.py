32b4751b0e63a405decae2c6d4b653a6d9014851
Vladyslav Lakhai <vlad.luckhi@gmail.com>
2022-09-03 15:09:37 +0300

    Improving Text Analyzer

--- homework/text_analyzer/main.py
+++
@@ -1,8 +0,0 @@
-from text_analyzer import TextAnalyzer
-
-
-if __name__ == '__main__':
-    text_filename = 'text'
-    text_analyzer = TextAnalyzer(filename=text_filename)
-    analysis_report = text_analyzer.generate_analysis_report()
-    print(analysis_report)
